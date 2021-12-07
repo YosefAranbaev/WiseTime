811d796633792cf364afec2ca5b112802929163e
karina <karina4nosenko@gmail.com>
2021-12-07 16:39:46 +0200

    Auth and calendar routers, get all events from microsoft api

--- wisetime-backend/controllers/authController.js
+++ wisetime-backend/controllers/authController.js
@@ -1,11 +1,63 @@
+const graph = require('../graph');
+
 exports.authController = {
-    signIn(req, res) {
-        res.send('signin');
+    async signIn(req, res) {
+        const urlParameters = {
+            scopes: process.env.OAUTH_SCOPES.split(','),
+            redirectUri: process.env.OAUTH_REDIRECT_URI
+        };
+
+        try {
+            const authUrl = await req.app.locals.msalClient.getAuthCodeUrl(urlParameters);
+            res.redirect(authUrl);
+        } catch (error) {
+            res.status(500).json({ 'error': 'Error while redirecting to Ms Authentication' });
+        }
     },
-    handleToken(req, res) {
-        res.send('handle token');
+    async getToken(req, res) {
+        const tokenRequest = {
+            code: req.query.code,
+            scopes: process.env.OAUTH_SCOPES.split(','),
+            redirectUri: process.env.OAUTH_REDIRECT_URI
+        };
+
+        try {
+            const response = await req.app.locals.msalClient.acquireTokenByCode(tokenRequest);
+            req.session.userId = response.account.homeAccountId;
+
+            const user = await graph.getUserDetails(
+                req.app.locals.msalClient,
+                req.session.userId
+            );
+
+            req.app.locals.users[req.session.userId] = {
+                displayName: user.displayName,
+                email: user.mail || user.userPrincipalName,
+                timeZone: user.mailboxSettings.timeZone
+            };
+        } catch {
+            console.log({ 'error': 'Error while acquiring token by code and adding user to user storage.' });
+        }
+
+        res.redirect('/');
     },
-    signOut(req, res) {
-        res.send('signout');
+    async signOut(req, res) {
+        if (req.session.userId) {
+            const accounts = await req.app.locals.msalClient
+                .getTokenCache()
+                .getAllAccounts();
+
+            const userAccount = accounts.find(account => account.homeAccountId === req.session.userId);
+
+            if (userAccount) {
+                req.app.locals.msalClient
+                    .getTokenCache()
+                    .removeAccount(userAccount);
+            }
+
+            req.session.destroy(() => {
+                res.redirect('/');
+            });
+        }
     }
 };
--- wisetime-backend/routers/authRouter.js
+++ wisetime-backend/routers/authRouter.js
@@ -4,7 +4,7 @@ const { authController } = require('../controllers/authController');
 const authRouter = new Router();
 
 authRouter.get('/signin', authController.signIn);
-authRouter.get('/callback', authController.handleToken);
+authRouter.get('/callback', authController.getToken);
 authRouter.get('/signout', authController.signOut);
 
 module.exports = { authRouter };
